c05c3ee947df1feaef114ec09552b35bdbba276e
Vaibhav Shinde <vaibhavkshinde20@gmail.com>
2021-09-21 18:38:58 +0530

    fix: build fail for /api

--- src/pages/api.jsx
+++ src/pages/api.jsx
@@ -3,24 +3,31 @@ import Layout from '@site/src/theme/Layout';
 
 import { API } from '@stoplight/elements';
 import Head from '@docusaurus/Head';
+import BrowserOnly from '@docusaurus/BrowserOnly';
 
 export default function APIPage(props) {
   return (
-    <Layout>
-      <Head>
-        {/* Loading the styles like this instead as it resets styles of many HTML tags
+    <BrowserOnly fallback={<div>Loading...</div>}>
+      {() => {
+        return (
+          <Layout>
+            <Head>
+              {/* Loading the styles like this instead as it resets styles of many HTML tags
         required for docs */}
-        <link
-          rel="stylesheet"
-          href="https://unpkg.com/@stoplight/elements/styles.min.css"
-        />
-      </Head>
-      <API
-        apiDescriptionUrl="/openapi.yaml"
-        router="hash"
-        basePath="/api"
-        layout="sidebar"
-      />
-    </Layout>
+              <link
+                rel="stylesheet"
+                href="https://unpkg.com/@stoplight/elements/styles.min.css"
+              />
+            </Head>
+            <API
+              apiDescriptionUrl="/openapi.yaml"
+              router="hash"
+              basePath="/api"
+              layout="sidebar"
+            />
+          </Layout>
+        );
+      }}
+    </BrowserOnly>
   );
 }
