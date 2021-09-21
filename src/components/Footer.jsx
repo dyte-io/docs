3fef0b20bb597edde724455eab2d22174731e3b6
Vaibhav Shinde <vaibhavkshinde20@gmail.com>
2021-09-21 15:11:54 +0530

    fix(ui): update GridLinks UI in introduction page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -33,7 +33,7 @@ export default function Footer(props) {
                 type="text"
                 name="email"
                 placeholder="Enter your email"
-                className="flex-1 h-10 bg-background-100 text-text text-sm border border-r-0 border-border rounded-l-md focus:ring-0"
+                className="flex-1 h-10 px-3 bg-background-100 text-text text-sm border border-r-0 border-border rounded-l-md focus:ring-0"
               />
               <button
                 type="submit"
--- src/components/GridLink.jsx
+++ src/components/GridLink.jsx
@@ -7,25 +7,28 @@ export default function GridLink({
   title,
   subtitle,
   className,
+  iconClassName,
   ...props
 }) {
   return (
     <Link
       className={clsx(
-        'flex items-center p-4 rounded-md bg-background-200 text-text break-words hover:text-text border-2 border-border transition hover:border-primary hover:no-underline hover:dyte-shadow',
+        'flex items-center p-6 rounded-md bg-background-100 text-text break-words hover:text-text border-2 border-border transition hover:border-primary hover:no-underline hover:dyte-shadow',
         className
       )}
       {...props}
     >
       {typeof Icon === 'string' ? (
-        <img src={Icon} className="h-10 md:h-12 mr-3" />
+        <img src={Icon} className={clsx('h-8 mr-6', iconClassName)} />
       ) : (
-        <Icon className="h-12 md:h-16 mr-3 text-primary" />
+        <Icon
+          className={clsx('h-12 md:h-16 mr-4 text-primary', iconClassName)}
+        />
       )}
 
       <div className="flex flex-col space-y-px">
-        <div className="font-medium">{title}</div>
-        <p className="text-xs text-text-100 m-0">{subtitle}</p>
+        {title && <div className="font-medium">{title}</div>}
+        {subtitle && <p className="text-xs text-text-100 m-0">{subtitle}</p>}
       </div>
     </Link>
   );
