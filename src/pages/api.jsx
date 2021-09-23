8aea366525c0dad77394c91edf794b9314046bcb
Vaibhav Shinde <vaibhavkshinde20@gmail.com>
2021-09-23 18:20:53 +0530

    feat: added fallback loading indicator for /api

--- src/pages/api.jsx
+++ src/pages/api.jsx
@@ -7,27 +7,36 @@ import BrowserOnly from '@docusaurus/BrowserOnly';
 
 export default function APIPage(props) {
   return (
-    <BrowserOnly>
-      {() => {
-        return (
-          <Layout>
-            <Head>
-              {/* Loading the styles like this instead as it resets styles of many HTML tags
+    <Layout>
+      <Head>
+        {/* Loading the styles like this instead as it resets styles of many HTML tags
         required for docs */}
-              <link
-                rel="stylesheet"
-                href="https://unpkg.com/@stoplight/elements/styles.min.css"
-              />
-            </Head>
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/@stoplight/elements/styles.min.css"
+        />
+      </Head>
+      <BrowserOnly
+        fallback={
+          <div className="flex items-center justify-center w-full min-h-screen">
+            <div
+              className="w-10 h-10 rounded-full border-l border-t-2 border-primary animate-spin"
+              aria-label="Loading..."
+            ></div>
+          </div>
+        }
+      >
+        {() => {
+          return (
             <API
               apiDescriptionUrl="/openapi.yaml"
               router="hash"
               basePath="/"
               layout="sidebar"
             />
-          </Layout>
-        );
-      }}
-    </BrowserOnly>
+          );
+        }}
+      </BrowserOnly>
+    </Layout>
   );
 }
