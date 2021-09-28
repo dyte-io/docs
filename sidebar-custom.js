3c6d64ac5c57e8db70d290d74ca1be4cedc1e581
akshaybhalotia <abhalotia99@gmail.com>
2021-09-28 21:11:38 +0530

    update sidebar

--- sidebar-custom.js
+++ sidebar-custom.js
@@ -3,7 +3,29 @@ module.exports = {
         {
             type: 'category',
             label: 'Home',
-            items: ['home/introduction'],
+            items: [
+                'home/introduction',
+                {
+                    type: 'link',
+                    label: 'How Dyte works',
+                    href: '/home/introduction#how-dyte-works'
+                },
+                {
+                    type: 'link',
+                    label: 'Supported Platforms',
+                    href: '/home/introduction#pick-a-platform-or-framework'
+                },
+                {
+                    type: 'link',
+                    label: 'API',
+                    href: '/home/introduction#read-about-our-server-apis'
+                },
+                {
+                    type: 'link',
+                    label: 'How to use the docs',
+                    href: '/home/introduction#how-to-use-these-docs'
+                }
+            ],
         },
         {
             type: 'category',
@@ -13,7 +35,38 @@ module.exports = {
                     type: 'link',
                     label: 'Installation',
                     href: '/react/installation'
-                }
+                },
+                {
+                    type: 'link',
+                    label: 'Usage',
+                    href: '/react/usage'
+                },
+                {
+                    type: 'link',
+                    label: 'Sample app',
+                    href: '/react/sample-app'
+                },
+                {
+                    type: 'link',
+                    label: 'Reference',
+                    href: '/react/reference/dyte-meeting'
+                },
+            ]
+        },
+        {
+            type: 'category',
+            label: 'API Reference',
+            items: [
+                {
+                    type: 'link',
+                    label: 'Overview',
+                    href: '/api'
+                },
+                {
+                    type: 'link',
+                    label: 'Entities',
+                    href: '/api/schemas'
+                },
             ]
         },
     ],
