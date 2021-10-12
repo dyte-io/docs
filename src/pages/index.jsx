d7bd790d4c438763eb811ba65c283c8a42363600
Vaibhav Shinde <vaibhavkshinde20@gmail.com>
2021-10-12 12:30:39 +0530

    fix(docs): updated images and fixed metadata content

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,24 @@
-import React from "react";
-import { Redirect } from "@docusaurus/router";
+import React from 'react';
+import { Redirect } from '@docusaurus/router';
+import Head from '@docusaurus/Head';
 
 export default function Homepage(props) {
-  return <Redirect to="/docs/home/introduction" />;
+  return (
+    <>
+      <Head>
+        <meta title="Dyte Docs" />
+        <meta property="og:title" content="Dyte Docs" />
+        <meta
+          property="og:description"
+          content="Real-time audio & video SDKs, ready to launch 🚀"
+        />
+        <meta
+          property="description"
+          content="Real-time audio & video SDKs, ready to launch 🚀"
+        />
+        <link rel="canonical" href="https://docs.dyte.io" />
+      </Head>
+      <Redirect to="/docs/home/introduction" />;
+    </>
+  );
 }
