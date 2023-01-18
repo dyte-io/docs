45634ee327f784f68d7e2cf29852f154fb95eb24
Palash <pgolecha12@gmail.com>
2023-01-18 16:16:57 +0530

    feat: show latest versions of all packages

---
+++ src/components/LatestInstallation.jsx
@@ -0,0 +1,105 @@
+import React, { useEffect, useState } from 'react';
+import CodeBlock from '@theme/CodeBlock';
+import latestNPMVersion from '../utils/npm';
+
+export const AndroidCoreLatestInstallation = () => {
+  const [version, setVersion] = useState('+');
+
+  useEffect(() => {
+    const raw = JSON.stringify({
+      "androidCore": true
+    });
+    
+    const requestOptions = {
+      method: 'POST',
+      body: raw,
+    };
+    
+    fetch("https://b72qj023g7.execute-api.ap-south-1.amazonaws.com/default/android-core-latest", requestOptions)
+      .then(response => response.json())
+      .then(result => setVersion(result.latestVersion ?? '+'))
+      .catch((e) => {});
+  }, []);
+  return (
+    <div>
+      <CodeBlock
+        language="groovy">
+        {`dependencies {
+    // (other dependencies)
+    implementation 'io.dyte:core:${version}'
+}`}
+      </CodeBlock>
+    </div>
+  );
+}
+
+
+export const WebCoreCDNInstallation = () => {
+  const [version,setVersion] = useState('');
+  useEffect(() => {
+    async function load() {
+      const version = await latestNPMVersion({ pkg: 'web-core' });
+      setVersion(`-${version}`);
+    }
+    load();
+  }, []);
+
+  return (
+  <CodeBlock
+        language="html">
+        {`<script src="https://cdn.dyte.in/core/dyte${version}.js" />`}
+  </CodeBlock>);
+};
+
+export const HTMLUIKitInstallation = () => {
+  const [webCoreVersion,setWebCoreVersion] = useState('');
+  const [uikitVersion,setUIKitVersion] = useState('');
+
+  useEffect(() => {
+    async function load() {
+      const versionWC = await latestNPMVersion({ pkg: 'web-core' });
+      setWebCoreVersion(`-${versionWC}`);
+      const versionUI = await latestNPMVersion({ pkg: 'ui-kit' });
+      setUIKitVersion(`@${versionUI}`);
+    }
+    load();
+  }, []);
+
+  return (
+  <CodeBlock
+        language="html">
+        {`<head>
+  <script type="module">
+      import { defineCustomElements } from 'https://cdn.jsdelivr.net/npm/@dytesdk/ui-kit${uikitVersion}/loader/index.es2017.js';
+      defineCustomElements();
+  </script>
+  <!-- Import Web Core via CDN too -->
+  <script src="https://cdn.dyte.in/core/dyte${webCoreVersion}.js"></script>
+</head>`}
+  </CodeBlock>);
+};
+
+export const HTMLUIKitUtilsInstallation = () => {
+  const [uikitVersion,setUIKitVersion] = useState('');
+
+  useEffect(() => {
+    async function load() {
+      const versionUI = await latestNPMVersion({ pkg: 'ui-kit' });
+      setUIKitVersion(`@${versionUI}`);
+    }
+    load();
+  }, []);
+
+  return (
+  <CodeBlock
+        language="html">
+        {`<head>
+  <script type="module">
+    import {
+      provideDyteDesignSystem,
+      extendConfig,
+    } from 'https://cdn.jsdelivr.net/npm/@dytesdk/ui-kit${uikitVersion}/dist/esm/index.js';
+  </script>
+</head>`}
+  </CodeBlock>);
+};
