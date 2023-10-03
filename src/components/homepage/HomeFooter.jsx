37dafb490c0615431fbb22f417b46f8a0371dc1c
Rohan Mukherjee <roerohan@gmail.com>
2023-10-03 17:20:54 +0530

    docs(grammar): fix grammatical error in footer
    Changed "Your Safety is utmost important to us" to "Your safety is of utmost importance to us"

--- src/components/homepage/HomeFooter.jsx
+++ src/components/homepage/HomeFooter.jsx
@@ -87,7 +87,7 @@ function Safety({ className }) {
       )}
     >
       <div className="flex flex-1 place-items-center justify-center rounded-2xl bg-white px-4 py-6 font-jakarta font-bold text-gray-500">
-        Your Safety is utmost important to us
+        Your safety is of utmost importance to us
       </div>
       <div className="flex flex-1 items-center justify-around px-6">
         <img src="/img/soc-compliant-1.png" />
